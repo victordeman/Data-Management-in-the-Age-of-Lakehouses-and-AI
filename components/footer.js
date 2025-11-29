7486c6b40ed8d276cb0c32b82e5ec521f1ecd1a6
Dr.-Ing. Chukwuka Victor obionwu <victordeman@users.noreply.github.com>
2025-11-29 19:18:24 +0100

    Update footer.js

--- components/footer.js
+++ components/footer.js
@@ -2,9 +2,9 @@ class CustomFooter extends HTMLElement {
     connectedCallback() {
         this.innerHTML = `
             <footer class="bg-gray-800 text-white p-4 mt-8">
-                <div class="container mx-auto flex flex-col md:flex-row justify-between items-center">
+                <div class="container mx-auto flex flex-col items-center space-y-4 md:flex-row md:justify-between md:space-y-0">
                     <p>&copy; 2025 Victor Deman. All rights reserved.</p>
-                    <nav class="flex space-x-4 mt-2 md:mt-0">
+                    <nav class="flex flex-col space-y-2 md:flex-row md:space-y-0 md:space-x-4">
                         <a href="#" class="hover:text-secondary-500">Privacy Policy</a>
                         <a href="#" class="hover:text-secondary-500">Terms of Use</a>
                         <a href="https://github.com/victordeman/Data-Management-in-the-Age-of-Lakehouses-and-AI" class="hover:text-secondary-500">GitHub Repo</a>
